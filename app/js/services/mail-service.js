df941ceccab2e7d80b9c1d9868a6df2b758e265a
Cliff Canan <cliff@nooch.com>
2016-11-22 18:31:25 -0500

    • Added SweetAlert2 package to bower; • Removed blog/ and all pics inside which weren't needed

--- app/js/services/mail-service.js
+++ app/js/services/mail-service.js
@@ -1,22 +1,23 @@
 app.factory('mails', ['$http', function ($http) {
 
-  var path = 'data/mail/mails.json';
+    var path = 'data/mails.json';
 
-  var mails = $http.get(path).then(function (resp) {
-    return resp.data.mails;
-  });
+    var mails = $http.get(path).then(function (resp) {
+        return resp.data.mails;
+    });
 
-  var factory = {};
-  factory.all = function () {
-    return mails;
-  };
-  factory.get = function (id) {
-    return mails.then(function(mails){
-      for (var i = 0; i < mails.length; i++) {
-        if (mails[i].id == id) return mails[i];
-      }
-      return null;
-    })
-  };
-  return factory;
+    var factory = {};
+    factory.all = function () {
+        return mails;
+    };
+    factory.get = function (id) {
+        return mails.then(function (mails) {
+            for (var i = 0; i < mails.length; i++)
+            {
+                if (mails[i].id == id) return mails[i];
+            }
+            return null;
+        })
+    };
+    return factory;
 }]);
